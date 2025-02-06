65525257eb93771fe7269370e631c3e9293324d7
bobowg163 <bobowg@163.com>
2025-02-06 22:24:24 +0800

    添加样式完毕。

---
+++ app/src/main/java/com/example/replyapp/ui/theme/Shapes.kt
@@ -0,0 +1,19 @@
+package com.example.replyapp.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+/*
+项目:ReplyApp
+包名：com.example.replyapp.ui.theme
+作者: bobo
+发布日期及时间: 2025/2/6  09:17
+*/
+val shapes = Shapes(
+    extraSmall = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(24.dp),
+    extraLarge = RoundedCornerShape(32.dp)
+)
